2c46893f27a57bc61bca8bf01f3eda64260d8378
Victor Ondari <vic@Victors-MacBook-Pro.local>
2022-10-13 16:38:21 +0300

    stk push using daraja... and b2c, c2b

---
+++ daraja/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.sign_up, name='user_sign'),
+    path('dashboard', views.user_dashboard, name='user_dashboard'),
+    path('logins', views.sign_in, name='login_user'),
+]
